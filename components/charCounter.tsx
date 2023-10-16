ed2b376a4be3c3d9a2b5d6d1bc9ed768248f77b8
Kevin Gyori <gyorijonatan@gmail.com>
2023-10-16 13:16:51 +0100

    selected text counter

--- components/charCounter.tsx
+++ components/charCounter.tsx
@@ -1,22 +1,30 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Textarea } from "./ui/textarea";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { ClipboardCopy, Eraser } from "lucide-react";
 
 const CharCounter = () => {
   const [charCount, setCharCount] = useState(0);
   const [over280, setOver280] = useState(false);
   const [over4000, setOver4000] = useState(false);
   const [content, setContent] = useState("");
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const [selection, setSelection] = useState("");
+
+  useEffect(() => {
+    console.log(window?.getSelection()?.toString());
+  });
+
+
+
 
   useEffect(() => {
     if (charCount > 280) {
@@ -46,7 +54,11 @@ const CharCounter = () => {
   };
 
   return (
-    <div className="flex-col">
+    <div className="flex-col"
+      onKeyUp={() => {
+        setSelection(window?.getSelection()?.toString() ?? "");
+      }}
+    >
       <div className="flex gap-6">
         <Card
           className={cn(
@@ -83,25 +95,41 @@ const CharCounter = () => {
           </CardFooter> */}
         </Card>
       </div>
-      <Textarea
-        className="md:w-[60ch] mt-5 min-h-[200px] text-lg"
-        onChange={handleContentChange}
-        placeholder="Start typing!"
-        value={content}
-      />
+      <div className="flex flex-col gap-2">
+        <Textarea
+          className="md:w-[60ch] mt-5 min-h-[200px] text-lg"
+          onChange={handleContentChange}
+          placeholder="Start typing!"
+          value={content}
+          ref={ref}
+          onSelectCapture={() => {
+            setSelection(window?.getSelection()?.toString() ?? "");
+          }}
+          onMouseUpCapture={() => {
+            setSelection(window?.getSelection()?.toString() ?? "");
+          }}
+          onBlurCapture={() => {
+            console.log("blur", window?.getSelection()?.toString());
+            setSelection(window?.getSelection()?.toString() ?? "");
+          }}
+        />
+        <span className="text-gray-500" > {selection.length} characters selected </span >
+      </div>
       <div className="flex flex-col md:flex-row mt-5 gap-5">
         <Button
           className="flex-auto"
           variant="destructive"
           onClick={handleClear}
         >
+          <Eraser className="mr-2 h-4 w-4" />
           Clear
         </Button>
         <Button className="flex-auto" variant="default" onClick={handleCopy}>
+          <ClipboardCopy className="mr-2 h-4 w-4" />
           Copy
         </Button>
       </div>
-    </div>
+    </div >
   );
 };
 
