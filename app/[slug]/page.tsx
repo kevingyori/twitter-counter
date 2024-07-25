7fe80cd93a690ecc5e751c2c4569a536f75f7293
Kevin Gyori <gyorijonatan@gmail.com>
2024-07-25 10:27:20 +0100

    don't dynamically load list of tweets

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 import CharCounter from "@/components/charCounter";
 import Editor from "@/components/editor";
+import { ListOfTweets } from "@/components/listOfTweets";
 import { TweetName } from "@/components/tweetName";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
 const Toaster = dynamic(() => import("@/components/ui/toaster"));
-const ListOfTweets = dynamic(() => import("@/components/listOfTweets"), {
-  loading: () => (
-    <div className="flex flex-col gap-3 md:w-[675px] min-w-full md:min-w-1 fade-in animate-in duration-200">
-      <div className="bg-gray-100 w-full h-10 rounded animate-pulse" />
-      <div className="bg-gray-100 h-20 rounded-lg" />
-      <div className="bg-gray-100 h-20 rounded-lg" />
-    </div>
-  ),
-});
 
 export default function Home() {
   const [charCount, setCharCount] = useState(0);
